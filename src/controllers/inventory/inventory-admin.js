2879b9a3afca14096363b404246071c55853527c
daniel.silvaoliveira <daniel.silvaoliveira@e-hps.com>
2018-05-01 16:26:59 -0400

    inventory-admin

--- src/controllers/inventory/inventory-admin.js
+++ src/controllers/inventory/inventory-admin.js
@@ -1,71 +1,44 @@
 'use strict';
 
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { mongoDB } from '../../../config';
 
 const router = express.Router();
+const { collections: { userCollection } } = mongoDB;
 
 // add book
 router.post('/admin/books', (req, res) => {
-    MongoClient.connect(process.env.DB_URL, (err, client) => {
+    const { db } = req.app.locals;
+
+    db.collection(userCollection).insertOne(req.body, (err, result) => {
         if (err) return err;
-
-        const db = client.db(process.env.DB_NAME);
-        const collection = db.collection(process.env.BOOK_COLLECTION);
-
-        // inserting new book to MongoDB
-        collection.insert(req.body, (error, result) => {
-            if (err) return err;
-
-            res.send(result);
-        });
-
-        client.close();
+        res.send(result);
     });
 });
 
 
 // remove book
 router.delete('/admin/books', (req, res) => {
-    MongoClient.connect(process.env.DB_URL, (err, client) => {
+    const { db } = req.app.locals;
+
+    db.collection(userCollection).deleteOne(req.body, (err, result) => {
         if (err) return err;
-
-        const db = client.db(process.env.DB_NAME);
-        const collection = db.collection(process.env.BOOK_COLLECTION);
-
-        collection.remove(req.body, (error, result) => {
-            if (err) return err;
-
-            res.send(result);
-        });
-
-        client.close();
+        res.send(result);
     });
 });
 
 
 // update book
 router.put('/admin/books', (req, res) => {
-    MongoClient.connect(process.env.DB_URL, (err, client) => {
-        if (err) return err;
-
-        const db = client.db(process.env.DB_NAME);
-        const collection = db.collection(process.env.BOOK_COLLECTION);
-
-        // TODO: item to be updated might need to be changed from req.body
-        // TODO: set value needs to be added
-        collection.update(
-            req.body,
-            { $set: {}, $currentDate: { lastModified: true } },
-            (error, result) => {
-                if (err) return err;
-
-                res.send(result);
-            }
-        );
-
-        client.close();
-    });
+    const { db } = req.app.locals;
+    db.collection(userCollection).updateOne(
+        req.body,
+        { $set: {}, $currentDate: { lastModified: true } },
+        (err, result) => {
+            if (err) return err;
+            res.send(result);
+        }
+    );
 });
 
 export default router;
