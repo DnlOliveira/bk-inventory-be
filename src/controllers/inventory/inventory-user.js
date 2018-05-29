1cf8682d0aa88bb29fb21561b539dca995ed82b6
daniel.silvaoliveira <daniel.silvaoliveira@e-hps.com>
2018-05-29 15:56:30 -0400

    clean up

--- src/controllers/inventory/inventory-user.js
+++ src/controllers/inventory/inventory-user.js
@@ -1,7 +1,3 @@
-// TODO: add routes to save an item to users
-// favorites and route for comments to a book;
-// comment should be saved to user and book;
-
 'use strict';
 
 import express from 'express';
